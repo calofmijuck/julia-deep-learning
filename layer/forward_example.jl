2df269fdbd543bc7a53f83a037c01f860e232768
Sungchan Yi <calofmijuck@snu.ac.kr>
2021-01-17 22:18:29 +0900

    Sample network and forward on a network

---
+++ layer/forward_example.jl
@@ -0,0 +1,32 @@
+include("../util/activation.jl")
+
+import Main.Activation
+
+function init_network()::Dict{String, AbstractArray}
+    nn = Dict()
+    nn["W1"] = [0.1 0.2; 0.3 0.4; 0.5 0.6]
+    nn["W2"] = [0.1 0.2 0.3; 0.4 0.5 0.6]
+    nn["W3"] = [0.1 0.2; 0.3 0.4]
+    
+    nn["b1"] = [0.1, 0.2, 0.3]
+    nn["b2"] = [0.1, 0.2]
+    nn["b3"] = [0.1, 0.2]
+
+    return nn
+end
+
+function forward(nn::Dict{String, AbstractArray}, x::AbstractArray)
+    W1, W2, W3 = nn["W1"], nn["W2"], nn["W3"]
+    b1, b2, b3 = nn["b1"], nn["b2"], nn["b3"]
+
+    z1 = Activation.sigmoid(W1 * x + b1)
+    z2 = Activation.sigmoid(W2 * z1 + b2)
+    z3 = W3 * z2 + b3
+    
+    return z3
+end
+
+nn = init_network()
+x = [1.0, 0.5]
+y = forward(nn, x)
+print(y)
